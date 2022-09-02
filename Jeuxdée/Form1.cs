008449914dddec27fd608e9a893d6c918f147315
Thomas-Toulouse-Cegep <thomas_toulouse@yahoo.com>
2022-09-02 18:59:03 -0400

    Add project files.

---
+++ Jeuxdée/Form1.cs
@@ -0,0 +1,14 @@
+namespace Jeuxdée
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
